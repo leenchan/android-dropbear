041664b51846ecf05ad4320624bbe3d4c04331f3
leenchan <leenchan@gmail.com>
2021-04-30 18:34:45 +0800

    Update localoptions.h

--- localoptions.h
+++ localoptions.h
@@ -4,10 +4,10 @@
  */
 
 // Disable server password auth as crypt() isn't available under Android
-// #define DROPBEAR_SVR_PASSWORD_AUTH 0
+#define DROPBEAR_SVR_PASSWORD_AUTH 0
 
 // Disable client password auth as getpass() isn't available under Android
 // #define DROPBEAR_CLI_PASSWORD_AUTH 0
 
 // Speed up symmetrical ciphers and hashes at the expense of larger binaries
-// #define DROPBEAR_SMALL_CODE 0
+#define DROPBEAR_SMALL_CODE 0
